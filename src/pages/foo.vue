18992994f1e940b54b4bfb9411bb298f5ec49d5f
tangxianwen <1658172479@qq.com>
2022-04-14 21:31:27 +0800

    feat: init

---
+++ src/pages/foo.vue
@@ -0,0 +1,12 @@
+<script></script>
+<template>
+  <div>foo</div>
+  <div>
+    <router-link
+      class="m-3 text-sm btn"
+      to="/"
+    >
+      Back
+    </router-link>
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+const router = useRouter()
+const go = () => {
+  router.push('/foo')
+}
+</script>
+
+<template>
+  <div>
+    <div>index</div>
+    <div>
+      <router-link
+        class="m-3 text-sm btn"
+        to="/foo"
+      >
+        Go
+      </router-link>
+    </div>
+  </div>
+</template>
